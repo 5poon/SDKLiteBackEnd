e9596039f303dc79a84d17202483c3ecfffff1d4
Edward Chiang <edward.chiang@mycom-osi.com>
2026-01-28 18:24:25 +1300

    chore(project): Initialize Spring Boot project structure

---
+++ src/main/java/com/sdklite/backend/SDKLiteBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sdklite.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDKLiteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SDKLiteBackEndApplication.class, args);
+	}
+
+}
